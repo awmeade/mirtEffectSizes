6887890a086389c82b70e0c92e6a6bb1eda3e428
Adam Meade <awmeade.mail@gmail.com>
2016-03-19 13:07:27 -0400

    afternoon commit
    most of test level stuff done

--- polytomous DIF Effect sizes.R
+++ polytomous DIF Effect sizes.R
@@ -20,7 +20,7 @@ make.data <- function(N){
   d2[13:15, ] <- d1[13:15, ] + 1  # here is the DIF
   itemtype <- rep('graded', nrow(a))
   dataset1 <- simdata(a, d1, N, itemtype)
-  dataset2 <- simdata(a, d2, N, itemtype)
+  dataset2 <- simdata(a, d2, N, itemtype, mu=-1.0)
   dat <- rbind(dataset1, dataset2)
   return(dat)
 }
@@ -34,23 +34,40 @@ group <- c(rep('Ref', N), rep('Foc', N))
 
 
 
-foc.group <- 'Foc'
+#foc.group <- 'Foc'
 itemnames <- colnames(dat)
 anc.items.names <- itemnames[c(2,8,9,10,12)]
 #test.items <- c(1,2:7,11,13:15)
 model_anchor <- multipleGroup(dat, model = 1, group = group,
   invariance = c(anc.items.names, 'free_means', 'free_var'))  # sets mean of group 1 to 0 so ref as 2
-coef(model_anchor,simplify = TRUE)
 
-theta <- fscores(model_anchor, full.scores = TRUE)
-focal.theta <- theta[(N+1):2000,]
-focal.theta.mean <- mean(focal.theta)
+# get factor means from model
+coef <- coef(model_anchor,simplify = TRUE)
+theta.mean.1 <- coef[[1]][[2]]
+theta.mean.2 <- coef[[2]][[2]]
+theta.mean.dif <- theta.mean.2 - theta.mean.1 
+
+
+### focal thetas these will be input into the function, check their mean
+theta.obs <- fscores(model_anchor, full.scores = TRUE)
+focal.theta.obs <- theta.obs[1:1000,]
+focal.theta.mean.obs <- mean(focal.theta.obs)
+
+theta.mean.dif
+focal.theta.mean.obs
+#set mean difference
+if(round(focal.theta.mean.obs,1) == 0){
+  use.focal.theta.mean = theta.mean.dif
+}else{
+  use.focal.theta.mean = focal.theta.mean.obs
+}
+use.focal.theta.mean
 
 
 
-#### quadrature nodes - just using base ####
+#### quadrature nodes - just using base R for this ####
 theta.normal   <- seq(-4,4,length=30)
-theta.den   <- dnorm(theta.normal,mean=focal.theta.mean, sd=1)
+theta.den   <- dnorm(theta.normal,mean=use.focal.theta.mean, sd=1)
 theta.density <- theta.den / sum(theta.den)
 rm(theta.den)
 
@@ -64,8 +81,8 @@ list.item_ES_ref.nrm <- list()
 for(i in 1:ncol(dat)){
   foc.extract<-extract.item(model_anchor,i,group=1)
   ref.extract<-extract.item(model_anchor,i,group=2)
-  foc.ES.obs <- expected.item(foc.extract,focal.theta)
-  ref.ES.obs <- expected.item(ref.extract,focal.theta)
+  foc.ES.obs <- expected.item(foc.extract,focal.theta.obs)
+  ref.ES.obs <- expected.item(ref.extract,focal.theta.obs)
   foc.ES.nrm <- expected.item(foc.extract,theta.normal)
   ref.ES.nrm <- expected.item(ref.extract,theta.normal)
   
@@ -74,6 +91,7 @@ for(i in 1:ncol(dat)){
   list.item_ES_foc.nrm[[i]] <- foc.ES.nrm
   list.item_ES_ref.nrm[[i]] <- ref.ES.nrm
 }
+rm(foc.extract,ref.extract,foc.ES.obs,ref.ES.obs,foc.ES.nrm,ref.ES.nrm)
 # put these in dataframe
 df.ref.obs <- do.call("cbind",list.item_ES_ref.obs) 
 df.foc.obs <- do.call("cbind",list.item_ES_foc.obs) 
@@ -87,36 +105,23 @@ df.foc.nrm
 df.ref.nrm
 
 #### means for each item in dataframe
-mean.ES.foc <- colMeans(df.foc.obs)
-mean.ES.ref <- colMeans(df.ref.obs)
+#mean.ES.foc <- colMeans(df.foc.obs)
+#mean.ES.ref <- colMeans(df.ref.obs)
 
 ### dataframe of observed difference scores
 df.dif.obs <- df.ref.obs - df.foc.obs
 df.abs.dif.obs <- abs(df.dif.obs)
-SIDS <- colMeans(df.dif)
-UIDS <- colMeans(df.abs.dif)
+SIDS <- colMeans(df.dif.obs)
+UIDS <- colMeans(df.abs.dif.obs)
 SIDS
 UIDS
-
-
-#### dataframe of normal dist difference scocres
-df.dif.nrm <- df.ref.nrm - df.foc.nrm
-df.abs.dif.nrm <- abs(df.dif.nrm)
-
-# weight normal by density
-weighted.nrm <- apply(df.dif.nrm,2, function(x) x*theta.density)
-weighted.abs.nrm <- apply(df.abs.dif.nrm,2, function(x) x*theta.density)
-SIDN <- colSums(weighted.nrm)           #now sum these
-UIDN <- colSums(weighted.abs.nrm)
-SIDN
-UIDN
 
 ################## MAX D section ################
 get.max.D <- function(f.d){
   f.d.abs <- abs(f.d)
   f.max.D.location <- which.max(f.d.abs) #which.max returns location
   f.max.D <- f.d[f.max.D.location]
-  f.max.D.theta <-  focal.theta[f.max.D.location] 
+  f.max.D.theta <-  focal.theta.obs[f.max.D.location] 
   f.df.d.max <- c(f.max.D.theta,f.max.D)
   return(f.df.d.max)
 }
@@ -127,31 +132,87 @@ list.max.D
 
 ############# Cohen D section ###########
 #pooled SD
-v.sd.foc <- apply(df.foc.obs,2,sd) # get SD per sample
-v.sd.ref <- apply(df.ref.obs,2,sd)
+# v.sd.foc <- apply(df.foc.obs,2,sd) # get SD per sample
+# v.sd.ref <- apply(df.ref.obs,2,sd)
+# 
+# f2 <- function(f2.d, f2.n){
+#     f2.return <- (f2.d^2)*(f2.n-1)
+#     return(f2.return)
+# } 
+# f.numerator.1 <- sapply(v.sd.foc,f2,N)
+# f.numerator.2 <- sapply(v.sd.ref,f2,N)
+# f.numerator <- f.numerator.1 + f.numerator.2
+# f.denom <- 2*N - 2
+# f.pooled.SD <- sqrt(f.numerator/f.denom)
+# 
+# v.mean.ES.foc.obs <- colMeans(df.foc.obs)
+# v.mean.ES.ref.obs <- colMeans(df.ref.obs)
+# v.ESSD <- (v.mean.ES.foc.obs - v.mean.ES.ref.obs) / f.pooled.SD  
+# rm(f.numerator.1,f.numerator.2,f.numerator,f.denom,f.pooled.SD)
 
-f2 <- function(f2.d, f2.n){
-    f2.return <- (f2.d^2)*(f2.n-1)
-    return(f2.return)
-} 
-f.numerator.1 <- sapply(v.sd.foc,f2,N)
-f.numerator.2 <- sapply(v.sd.ref,f2,N)
-f.numerator <- f.numerator.1 + f.numerator.2
-f.denom <- 2*N - 2
-f.pooled.SD <- sqrt(f.numerator/f.denom)
-
-v.mean.ES.foc.obs <- colMeans(df.foc.obs)
-v.mean.ES.ref.obs <- colMeans(df.ref.obs)
-v.ESSD <- (v.mean.ES.ref.obs - v.mean.ES.foc.obs) / f.pooled.SD  
-rm(f.numerator.1,f.numerator.2,f.numerator,f.denom,f.pooled.SD)
-
+f.cohen.d <- function (vector.1,vector.2){
+  f.mean.v1 <- mean(vector.1)
+  f.mean.v2 <- mean(vector.2)
+  f.sd.v1 <- sd(vector.1)
+  f.sd.v2 <- sd(vector.2)
+  f.n.1 <- length(vector.1)
+  f.n.2 <- length(vector.2)
+  f.numerator <- (f.sd.v1^2)*(f.n.1 - 1) + (f.sd.v2^2)*(f.n.2 - 1)
+  f.denom <- f.n.1 + f.n.2 - 2
+  f.sd.pooled <- f.numerator / f.denom
+  f.return <- (f.mean.v2 - f.mean.v1) / f.sd.pooled
+  return(f.return)
+}
 
 
-###### Test section #######
-test.ES.foc.obs <- rowMeans(df.foc.obs)
-test.ES.ref.obs <- rowMeans(df.ref.obs)
-test.ES.dif.obs <- test.ES.ref.obs - test.ES.foc.obs
+list.item_CohenD.obs <- list()
+for(i in 1:ncol(dat)){
+  list.item_CohenD.obs[i] <- f.cohen.d(df.foc.obs[,i],df.ref.obs[,i])
+}
+#ESSD <- do.call("cbind",list.item_CohenD.obs) #dataframe 
+ESSD <- unlist(list.item_CohenD.obs) #vector
+# need to weight normal one
+
+
+###### Test level observed  #######
+STDS <- sum(SIDS)
+UTDS <- sum(UIDS)
+test.ES.foc.obs <- rowSums(df.foc.obs)  ### avg across items
+test.ES.ref.obs <- rowSums(df.ref.obs)
+test.ES.dif.obs <- test.ES.foc.obs - test.ES.ref.obs
 test.ES.dif.abs.obs <- abs(test.ES.dif.obs)
+UETSDS <- mean(test.ES.dif.abs.obs)
 
+ETSSD <- f.cohen.d(test.ES.ref.obs, test.ES.foc.obs) # cohen's D
+ETSSD
+
+
+
+
+
+
+
+############################### normal distribution ####################
+#### dataframe of normal dist difference scocres
+df.dif.nrm <- df.foc.nrm - df.ref.nrm
+df.abs.dif.nrm <- abs(df.dif.nrm)
+
+# weight normal by density
+weighted.dif.nrm <- apply(df.dif.nrm,2, function(x) x*theta.density)
+weighted.dif.abs.nrm <- apply(df.abs.dif.nrm,2, function(x) x*theta.density)
+SIDN <- colSums(weighted.nrm)           #now sum these
+UIDN <- colSums(weighted.abs.nrm)
+SIDN
+UIDN
+
+test.ES.foc.nrm <- rowSums(df.foc.nrm)  ### sum across items at each theta
+test.ES.ref.nrm <- rowSums(df.ref.nrm)  ### sum across items at each theta
+test.ES.dif.nrm <- rowSums(df.dif.nrm)  ### dif at each theta
+
+#test.dif.nrm <- rowSums(df.dif.nrm)  # sum across items
+test.dif.nrm.weighted <- test.ES.dif.nrm*theta.density
+Starks.DTF <- sum(test.dif.nrm.weighted)
+
+#test.dif.abs.nrm <- rowMeans(df.abs.dif.nrm) #avg across items
 
 
